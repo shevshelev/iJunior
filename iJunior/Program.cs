86a9f3a638ed521c44f418032dfd06a2f1f995df
Shevshelev Lev <lshevshelev@alfabank.ru>
2024-04-18 21:02:25 +0500

    Work with strings

--- iJunior/Program.cs
+++ iJunior/Program.cs
@@ -4,7 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string name;
+        string planet;
+        string age;
+
+        Console.WriteLine("Имя введи своё");
+        name = Console.ReadLine();
+        Console.WriteLine("Планеты ты с какой?");
+        planet = Console.ReadLine();
+        Console.WriteLine("Возраста каого ты?");
+        age = Console.ReadLine();
+
+        Console.WriteLine($"Поздравляю {name}, {age} лет от роду, с планеты {planet} тебя, джедай ты!");
     }
 }
 
