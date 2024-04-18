274bec972f8f08469f1c6824e9e03140decb1776
Shevshelev Lev <lshevshelev@alfabank.ru>
2024-04-18 19:13:48 +0500

    Variables

---
+++ iJunior/Variables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace iJunior
+{
+	public class Variables
+	{
+        float health = 100f;
+        double power = 56.67;
+        uint armor = 23;
+        int numberOfCats = 72;
+        string characterName = "Alf";
+        string race = "Melmokian";
+        string favoriteDish = "Cat";
+        bool isHungry = true;
+        bool isActive = true;
+        bool isHidden = false;
+    }
+}
