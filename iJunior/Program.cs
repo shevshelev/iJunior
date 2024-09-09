b62c5983f36ac9c05bbd53b19663b028bbb4fe89
Shevshelev Lev <lshevshelev@alfabank.ru>
2024-09-09 11:17:00 +0500

    Sum fix

--- iJunior/Program.cs
+++ iJunior/Program.cs
@@ -4,13 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random rand = new Random();
-        int number = rand.Next(0, 100);
+        Random random = new Random();
+        int minimum = 0;
+        int maximum = 100;
+        int firstDivider = 3;
+        int secondDivider = 5;
+        int number = random.Next(minimum, maximum);
         int sum = 0;
 
 
         for (int i = 0; i <= number; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
+            if (i % firstDivider == 0 || i % secondDivider == 0) {
                 sum += i;
             }
         }
