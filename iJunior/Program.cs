2c8436cf102f7a500308885464bd79f60520b671
Shevshelev Lev <lshevshelev@alfabank.ru>
2024-09-09 20:53:34 +0500

    Currency converter

--- iJunior/Program.cs
+++ iJunior/Program.cs
@@ -1,62 +1,185 @@
-﻿namespace iJunior;
+namespace iJunior;
 
 class Program
 {
     static void Main(string[] args)
     {
-        const string CommandShowGreeting = "1";
-        const string CommandShowGoodbay = "2";
-        const string CommandShowRandomNumber = "3";
-        const string CommandClean = "4";
-        const string CommandExit = "5";
+        const string CommandConvertRUBToUSD = "1";
+        const string CommandConvertUSDToRUB = "2";
+        const string CommandConvertRUBToEUR = "3";
+        const string CommandConvertEURToRUB = "4";
+        const string CommandConvertUSDToEUR = "5";
+        const string CommandConvertEURToUSD = "6";
+        const string CommandExit = "7";
 
-        string commandsText = "Available commands:";
-        string greetingText = "Show greeting";
-        string goodbayText = "Show goodbay";
-        string randomNumberText = "Show random number";
-        string cleanText = "Clean console";
-        string exitTextCommand = "Exit";
-        string enterCommand = "Enter command number:";
+        string greeting = "Hellol, you have:";
+        string availableExchange = "\nAvailable exchanges:";
+        string enterExchange = "Please enter exchange command.";
+        string RUBText = "RUB";
+        string USDText = "USD";
+        string EURText = "EUR";
+        string RUBToUSDText = $"Convert {RUBText} to {USDText}";
+        string USDToRUBText = $"Convert {USDText} to {RUBText}";
+        string RUBToEURText = $"Convert {RUBText} to {EURText}";
+        string EURToRUBText = $"Convert {EURText} to {RUBText}";
+        string USDToEURText = $"Convert {USDText} to {EURText}";
+        string EURToUSDText = $"Convert {EURText} to {USDText}";
+        string RUBExchangeText = $"How many {RUBText} do you want exchange?";
+        string USDExchangeText = $"How many {USDText} do you want exchange?";
+        string EURExchangeText = $"How many {EURText} do you want exchange?";
+        string exitText = "Exit";
+        string completed = "Operation completed.";
+        string error = "The specified amount exceeds amount in account.";
+        string anyKey = "Please press any key.";
+        string programCompleted = "Program completed";
+        string unknownCommand = "Unknown command";
 
-        string greeting = "Hi There!!!";
-        string goodbay = "Goodbay!";
-        string exitText = "Program completed";
-        string unknownCommand = "Unknown comand";
-        int minLimitForRandomNumber = 0;
-        int maxLimitForRandomNumber = 1000;
+        double USDAccount = 123;
+        double RUBAccount = 100_000;
+        double EURAccount = 6_000_000;
 
-        Random random= new Random();
+        double USDToRUBRate = 100;
+        double EURToRUBRate = 120;
+        double EURToUSDRate = EURToRUBRate / USDToRUBRate;
+        double RUBToUSDRate = 1 / USDToRUBRate;
+        double RUBToEURRate = 1 / EURToRUBRate;
+        double USDToEURRate = 1 /EURToUSDRate ;
+
         bool isWork = true;
+
         string userInput;
 
         while (isWork) {
-            Console.WriteLine(commandsText);
-            Console.WriteLine($"{CommandShowGreeting}. {greetingText}");
-            Console.WriteLine($"{CommandShowGoodbay}. {goodbayText}");
-            Console.WriteLine($"{CommandShowRandomNumber}. {randomNumberText}");
-            Console.WriteLine($"{CommandClean}. {cleanText}");
-            Console.WriteLine($"{CommandExit}. {exitTextCommand}");
-            Console.WriteLine(enterCommand);
+            double desiredAmount;
+
+            Console.WriteLine(greeting);
+            Console.WriteLine($"{USDAccount} {USDText}");
+            Console.WriteLine($"{RUBAccount} {RUBText}");
+            Console.WriteLine($"{EURAccount} {EURText}");
+            Console.WriteLine(availableExchange);
+            Console.WriteLine($"{CommandConvertRUBToUSD}. {RUBToUSDText}");
+            Console.WriteLine($"{CommandConvertUSDToRUB}. {USDToRUBText}");
+            Console.WriteLine($"{CommandConvertRUBToEUR}. {RUBToEURText}");
+            Console.WriteLine($"{CommandConvertEURToRUB}. {EURToRUBText}");
+            Console.WriteLine($"{CommandConvertUSDToEUR}. {USDToEURText}");
+            Console.WriteLine($"{CommandConvertEURToUSD}. {EURToUSDText}");
+            Console.WriteLine($"{CommandExit}. {exitText}");
+            Console.Write(enterExchange);
 
             userInput = Console.ReadLine();
 
             switch (userInput) {
-                case CommandShowGreeting:
-                    Console.WriteLine(greeting);
+                case CommandConvertRUBToUSD:
+                    Console.WriteLine(RUBExchangeText);
+
+                    desiredAmount = Convert.ToDouble(Console.ReadLine());
+
+                    if (RUBAccount >= desiredAmount) {
+                        RUBAccount -= desiredAmount;
+                        USDAccount += desiredAmount * RUBToUSDRate;
+
+                        Console.WriteLine(completed);
+                        Console.WriteLine(anyKey);
+                        Console.ReadKey();
+                    } else {
+                        Console.WriteLine(error);
+                    }
+
                     break;
-                case CommandShowGoodbay:
-                    Console.WriteLine(goodbay);
+
+                case CommandConvertUSDToRUB:
+                    Console.WriteLine(USDExchangeText);
+
+                    desiredAmount = Convert.ToDouble(Console.ReadLine());
+
+                    if (USDAccount >= desiredAmount) {
+                        USDAccount -= desiredAmount;
+                        RUBAccount += desiredAmount * USDToRUBRate;
+
+                        Console.WriteLine(completed);
+                        Console.WriteLine(anyKey);
+                        Console.ReadKey();
+                    } else {
+                        Console.WriteLine(error);
+                    }
+
                     break;
-                case CommandShowRandomNumber:
-                    int number = random.Next(minLimitForRandomNumber, maxLimitForRandomNumber);
-                    Console.WriteLine(number);
+
+                case CommandConvertRUBToEUR:
+                    Console.WriteLine(RUBExchangeText);
+
+                    desiredAmount = Convert.ToDouble(Console.ReadLine());
+
+                    if (RUBAccount >= desiredAmount) {
+                        RUBAccount -= desiredAmount;
+                        EURAccount += desiredAmount * RUBToEURRate;
+
+                        Console.WriteLine(completed);
+                        Console.WriteLine(anyKey);
+                        Console.ReadKey();
+                    } else {
+                        Console.WriteLine(error);
+                    }
+
                     break;
-                case CommandClean:
-                    Console.Clear();
+
+                case CommandConvertEURToRUB:
+                    Console.WriteLine(EURExchangeText);
+
+                    desiredAmount = Convert.ToDouble(Console.ReadLine());
+
+                    if (EURAccount >= desiredAmount) {
+                        EURAccount -= desiredAmount;
+                        RUBAccount += desiredAmount * EURToRUBRate;
+
+                        Console.WriteLine(completed);
+                        Console.WriteLine(anyKey);
+                        Console.ReadKey();
+                    } else {
+                        Console.WriteLine(error);
+                    }
+
                     break;
+
+                case CommandConvertUSDToEUR:
+                    Console.WriteLine(USDExchangeText);
+
+                    desiredAmount = Convert.ToDouble(Console.ReadLine());
+
+                    if (USDAccount >= desiredAmount) {
+                        USDAccount -= desiredAmount;
+                        EURAccount += desiredAmount * USDToEURRate;
+
+                        Console.WriteLine(completed);
+                        Console.WriteLine(anyKey);
+                        Console.ReadKey();
+                    } else {
+                        Console.WriteLine(error);
+                    }
+
+                    break;
+
+                case CommandConvertEURToUSD:
+                    Console.WriteLine(EURExchangeText);
+
+                    desiredAmount = Convert.ToDouble(Console.ReadLine());
+
+                    if (EURAccount >= desiredAmount) {
+                        EURAccount -= desiredAmount;
+                        USDAccount += desiredAmount * EURToUSDRate;
+
+                        Console.WriteLine(completed);
+                        Console.WriteLine(anyKey);
+                        Console.ReadKey();
+                    } else {
+                        Console.WriteLine(error);
+                    }
+
+                    break;
+
                 case CommandExit:
                     isWork = false;
-                    Console.WriteLine(exitText);
+                    Console.WriteLine(programCompleted);
                     break;
                 default:
                     Console.WriteLine(unknownCommand);
@@ -64,6 +187,7 @@ class Program
             }
         }
 
-        Console.ReadKey();
+
+        
     }
 }
