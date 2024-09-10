2ea223326b95d80cc036af9a752a2877adb8f7cf
Shevshelev Lev <lshevshelev@alfabank.ru>
2024-09-10 20:36:34 +0500

    Program with a password fix

--- iJunior/Program.cs
+++ iJunior/Program.cs
@@ -5,14 +5,14 @@ class Program
     static void Main(string[] args)
     {
         string userInput;
+        
         int attemptsNumber = 3;
-        int currentAttempt = 0;
         string passsword = "SomePassword";
         string secretMessage = "You are amazing!";
         string greetingMessage = "Please enter password";
         string incorrectMessage = "Password is incorrect. Please try again.";
 
-        while (currentAttempt < attemptsNumber)
+        for (int currentAttempt = 1; currentAttempt <= attemptsNumber; currentAttempt++)
         {
             Console.WriteLine(greetingMessage);
 
@@ -25,7 +25,6 @@ class Program
             } 
             else 
             {
-                currentAttempt += 1;
                 Console.WriteLine(incorrectMessage);
                 Console.WriteLine($"You have {attemptsNumber - currentAttempt} attempts left.");
                 continue;
